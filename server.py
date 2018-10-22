c4ece99e6d3398ee7ce9240c22c4ca30c55c48e5
Andrew <a1covker@gmail.com>
2018-10-22 13:16:23 +0100

    almostwitter skeleton

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__) # note there are two underscores
 @app.route('/') 
 @app.route('/index') 
 def index():
-    return "Hello, World!"
+     return render_template('index.html')
 
 if __name__ == '__main__': app.run()
