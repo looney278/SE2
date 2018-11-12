af57d903e430b187e935785020bd30b2b6e5ccf0
KieranW26 <44164830+KieranW26@users.noreply.github.com>
2018-11-12 15:25:24 +0000

    Add files via upload
    Registration is now working, though hash change each time... need to solve this, once that works, login will be "working" but need to add tokening/authentication kinda stuffs

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 from flask import *
 import psycopg2
 import re
+import hashlib, uuid
 
+salt = uuid.uuid4().hex
 emailpattern = re.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 passpattern = re.compile("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$")
 
-app = Flask(__name__)  # note there are two underscores
+app = Flask(__name__)
 
 
 def getconn():
-    connstr = "host='localhost' dbname= 'localhost' user ='postgre' password= 'password' "
+    connstr = "host='localhost' dbname='postgres' user='postgres' password='password'"
     conn = psycopg2.connect(connstr)
     return conn
 
@@ -20,6 +22,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
 @app.route('/registerUser', methods=['POST'])
 def registerUser():
     try:
@@ -27,8 +34,6 @@ def registerUser():
         username = request.form['username']
         firstname = request.form['firstName']
         lastname = request.form['lastName']
-        country = request.form['country']
-        county = request.form['county']
         email = request.form['email']
         confemail = request.form['confEmail']
         password = request.form['password']
@@ -38,26 +43,56 @@ def registerUser():
             return render_template('registration.html', emailError='Please use a valid email.')
         elif not email == confemail:
             return render_template('registration.html', emailError='Please ensure emails match.')
-        elif not passpattern == password:
-            return render_template('registration.html', passError='Password much contain at least 1 letter, 1 number'
-                                                                  'and be between 6-15 characters long.')
+        elif not passpattern.match(password):
+            return render_template('registration.html', passwordError='Password much contain at least 1 letter, '
+                                                                      '1 number and be between 6-15 characters long')
         elif not password == confpassword:
-            return render_template('registration.html', passError='Please ensure passwords match.')
+            return render_template('registration.html', passwordError='Please ensure passwords match.')
         else:
+            hashed_password = hashlib.sha512(password.encode() + salt.encode()).hexdigest()
             conn = getconn()
             cur = conn.cursor()
-            cur.execute('SET search_path to project')
+            cur.execute('SET search_path to assignment')
 
-            cur.execute('INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s, %s)', \
-                        [username, firstname, lastname, country, county, email, password])
+            cur.execute('INSERT INTO users VALUES (%s, %s, %s, %s, %s)', \
+                        [username, firstname, lastname, email, hashed_password])
             conn.commit()
-            return render_template('index.html', msg='Customer Added')
+            return render_template('index.html')
     except Exception as e:
-        return render_template('index.html', msg='Customer NOT Added ', error=e)
+        return render_template('index.html')
     finally:
         if conn:
             conn.close()
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/loginUser', methods=['GET', 'POST'])
+def loginUser():
+    conn = None
+    #try:
+    username = request.form['username']
+    sub_pass = request.form['password']
+    sub_hashed_password = hashlib.sha512(sub_pass.encode() + salt.encode()).hexdigest()
+    conn = getconn()
+    cur = conn.cursor()
+    cur.execute('SET search_path to assignment')
+
+    stored_pass = cur.execute('SELECT hashpassword FROM users where username = \'{0}\''.format(username))
+    if stored_pass == sub_hashed_password:
+        x=1
+        #CREATE COOKIE/TOKEN/LOGIN THINGY]
+    else:
+        return render_template('login.html', logerror = 'Username or Password is incorrect.')
+    return render_template('index.html')
+#except Exception as e:
+#    return render_template('index.html')
+#finally:
+    if conn:
+        conn.close()
 
 if __name__ == "__main__":
     app.run(ssl_context=('cert.pem', 'key.pem'))
