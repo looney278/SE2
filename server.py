176345247d75e7ee33a2a46f37d665c56c215a88
KieranW26 <44164830+KieranW26@users.noreply.github.com>
2018-11-16 11:11:43 +0000

    New Accounts page, password change
    Very simple accounts page added, password change feature added.

--- server.py
+++ server.py
@@ -8,9 +8,13 @@ import uuid
 emailpattern = re.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 # RegEx for password 6-15 characters, 1 number, 1 letter.
 passpattern = re.compile("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$")
+# RegEx for username 3-20 characters
+userpattern = re.compile("^(?=.{6,20}$)[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$")
 
 app = Flask(__name__)
 app.secret_key = 'mSMKW@Oa^8ingejvKj_<8Je1;_|Y&]n,J<^EK@!pupC=Mg.$;Df?q|`}a|FF_'
+
+search_path = 'SET search_path TO assignment'
 
 
 # creates initial connection to the database
@@ -53,12 +57,16 @@ def register_user():
         elif not passpattern.match(password):
             return render_template('registration.html', passwordError='Password much contain at least 1 letter, '
                                                                       '1 number and be between 6-15 characters long')
+        elif not userpattern.match(username):
+            return render_template('registration.html', usernameError='Username must be between 3-20 characters, '
+                                                                      'consist of alphanumerics, -, _ and spaces.'
+                                                                      'No more than 2 -, _ or spaces consecutively')
         elif not password == confpassword:
             return render_template('registration.html', passwordError='Please ensure passwords match.')
         else:
             conn = getconn()
             cur = conn.cursor()
-            cur.execute('SET search_path to assignment')
+            cur.execute(search_path)
 
             # duplicate username check
             cur.execute("SELECT username FROM users where username = (%s)", (username,))
@@ -85,6 +93,55 @@ def register_user():
             conn.close()
 
 
+@app.route('/account', methods=['GET'])
+def account():
+    conn = None
+    conn = getconn()
+    cur = conn.cursor()
+    cur.execute(search_path)
+    username = session["username"]
+    cur.execute('SELECT firstname, surname FROM users WHERE username = (%s)', (username,))
+    acc_username = username
+    name = str(cur.fetchone())
+    cur.execute('SELECT email FROM users WHERE username = (%s)', (username,))
+    email = str(cur.fetchone())
+    return render_template('account.html', acc_username=acc_username, name=name, email=email)
+
+
+@app.route('/change_pass')
+def change_pass():
+    return render_template('password_change.html')
+
+
+@app.route('/password_change', methods=['GET', 'POST'])
+def password_change():
+    try:
+        conn = getconn()
+        cur = conn.cursor()
+        cur.execute(search_path)
+        cur.execute('SELECT password FROM users WHERE username = (%s)', (session['username'],))
+        correct_old_pass = str(cur.fetchone()[0])
+        entered_old_pass = request.form['old_pass']
+        new_pass = request.form['new_pass']
+        conf_new_pass = request.form['conf_new_pass']
+        if new_pass is not conf_new_pass:
+            render_template('password_change.html', pass_error="New passwords must match")
+        cur.execute('SELECT salt FROM users WHERE username = (%s)', (session['username'],))
+        salt = str(cur.fetchone()[0])
+        hashed_password = hashlib.sha512(entered_old_pass.encode() + salt.encode()).hexdigest()
+        if hashed_password != correct_old_pass:
+            return render_template('password_change.html', pass_error="incorrect password")
+
+        new_hashed_password = hashlib.sha512(new_pass.encode() + salt.encode()).hexdigest()
+        cur.execute('UPDATE users SET password = (%s) WHERE username = (%s)',
+                    (new_hashed_password, session['username'],))
+        return render_template('account.html')
+    except Exception as e:
+        return render_template('error.html', FUCK=e)
+    finally:
+        if conn:
+            conn.close()
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -95,6 +152,7 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+
 @app.route('/login_user', methods=['GET', 'POST'])
 def login_user():
     try:
@@ -103,7 +161,7 @@ def login_user():
         cur = conn.cursor()
         username = request.form['username']
         entered_pass = request.form['password']
-        cur.execute('SET search_path to assignment')
+        cur.execute(search_path)
         # Takes the salt stored with the user name, adds and hashes the password provided then checks it against the
         # stored password
         cur.execute("SELECT password FROM users WHERE username = (%s);", (username,))
