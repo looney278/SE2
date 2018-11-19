32e73ccc064ab898e3ac5f0eee5708e13f50b655
KieranW26 <44164830+KieranW26@users.noreply.github.com>
2018-11-19 14:26:41 +0000

    All SQL updated to bad SQL, password reset might work?
    All SQL is not bad, as per Week 3 Lab, password reset should work but no commits seem to be going to the server will fix this later

--- server.py
+++ server.py
@@ -1,18 +1,34 @@
-from flask import Flask, session, redirect, url_for, escape, request, render_template
+from flask import Flask, session, redirect, url_for, request, render_template
+from flask_mail import Mail, Message
 import psycopg2
 import re
 import hashlib
 import uuid
+import random
+import string
 
 # RegEx for email.
-emailpattern = re.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+email_pattern = re.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 # RegEx for password 6-15 characters, 1 number, 1 letter.
-passpattern = re.compile("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$")
+pass_pattern = re.compile("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$")
 # RegEx for username 3-20 characters
-userpattern = re.compile("^(?=.{6,20}$)[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$")
+user_pattern = re.compile("^(?=.{6,20}$)[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$")
 
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'TestForCoursework@gmail.com',
+    "MAIL_PASSWORD": 'Potato555'
+}
 app = Flask(__name__)
-app.secret_key = 'mSMKW@Oa^8ingejvKj_<8Je1;_|Y&]n,J<^EK@!pupC=Mg.$;Df?q|`}a|FF_'
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+
+app.secret_key = 'mSMKW@Oa^8ingejvKj_<8Je1;_|Y&]n,J<^EK@!pupC=Mg.$;Df?query|`}a|FF_'
 
 search_path = 'SET search_path TO assignment'
 
@@ -50,14 +66,14 @@ def register_user():
         confpassword = request.form['confPassword']
 
         # checks all RegEx matches
-        if not emailpattern.match(email):
+        if not email_pattern.match(email):
             return render_template('registration.html', emailError='Please use a valid email.')
         elif not email == confemail:
             return render_template('registration.html', emailError='Please ensure emails match.')
-        elif not passpattern.match(password):
+        elif not pass_pattern.match(password):
             return render_template('registration.html', passwordError='Password much contain at least 1 letter, '
                                                                       '1 number and be between 6-15 characters long')
-        elif not userpattern.match(username):
+        elif not user_pattern.match(username):
             return render_template('registration.html', usernameError='Username must be between 3-20 characters, '
                                                                       'consist of alphanumerics, -, _ and spaces.'
                                                                       'No more than 2 -, _ or spaces consecutively')
@@ -69,12 +85,15 @@ def register_user():
             cur.execute(search_path)
 
             # duplicate username check
-            cur.execute("SELECT username FROM users where username = (%s)", (username,))
+            # cur.execute("SELECT username FROM users where username = (%s)", (username,))
+            cur.execute("SELECT username FROM users where username = '%s'" % username)
             if cur.fetchone() is not None:
                 return render_template('registration.html', usernameError='Username already exists')
 
             # duplicate email check
-            cur.execute("SELECT email FROM users where email = (%s)", (email,))
+            # cur.execute("SELECT email FROM users where email = (%s)", (email,))
+            cur.execute("SELECT email FROM users where email = '%s'" % email)
+
             if cur.fetchone() is not None:
                 return render_template('registration.html', emailError='Email already registered')
 
@@ -82,11 +101,15 @@ def register_user():
             salt = uuid.uuid4().hex
             hashed_password = hashlib.sha512(password.encode() + salt.encode()).hexdigest()
 
-            cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)", \
-                        [username, firstname, lastname, email, hashed_password, salt])
+            # cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)", \
+            #           [username, firstname, lastname, email, hashed_password, salt])
+
+            cur.execute("INSERT INTO users VALUES '%s', '%s','%s','%s','%s','%s'" %
+                        (username, firstname, lastname, email, hashed_password, salt))
             conn.commit()
             return render_template('index.html')
     except Exception as e:
+        print(e)
         return render_template('error.html', FUCK=e)
     finally:
         if conn:
@@ -95,17 +118,22 @@ def register_user():
 
 @app.route('/account', methods=['GET'])
 def account():
-    conn = None
-    conn = getconn()
-    cur = conn.cursor()
-    cur.execute(search_path)
-    username = session["username"]
-    cur.execute('SELECT firstname, surname FROM users WHERE username = (%s)', (username,))
-    acc_username = username
-    name = str(cur.fetchone())
-    cur.execute('SELECT email FROM users WHERE username = (%s)', (username,))
-    email = str(cur.fetchone())
-    return render_template('account.html', acc_username=acc_username, name=name, email=email)
+    try:
+        conn = getconn()
+        cur = conn.cursor()
+        cur.execute(search_path)
+        username = session["username"]
+        cur.execute("SELECT firstname, surname FROM users WHERE username = '%s'" % username)
+        acc_username = username
+        name = str(cur.fetchone())
+        cur.execute("SELECT email FROM users WHERE username = '%s'" % username)
+        email = str(cur.fetchone())
+        return render_template('account.html', acc_username=acc_username, name=name, email=email)
+    except Exception as e:
+        return render_template('error.html', FUCK=e)
+    finally:
+        if conn:
+            conn.close()
 
 
 @app.route('/change_pass')
@@ -116,31 +144,34 @@ def change_pass():
 @app.route('/password_change', methods=['GET', 'POST'])
 def password_change():
     try:
+        conn = None
         conn = getconn()
         cur = conn.cursor()
         cur.execute(search_path)
-        cur.execute('SELECT password FROM users WHERE username = (%s)', (session['username'],))
+        cur.execute("SELECT password FROM users WHERE username = '%s'" % session['username'])
         correct_old_pass = str(cur.fetchone()[0])
         entered_old_pass = request.form['old_pass']
         new_pass = request.form['new_pass']
         conf_new_pass = request.form['conf_new_pass']
         if new_pass is not conf_new_pass:
             render_template('password_change.html', pass_error="New passwords must match")
-        cur.execute('SELECT salt FROM users WHERE username = (%s)', (session['username'],))
+        cur.execute("SELECT salt FROM users WHERE username = '%s'" % session['username'])
         salt = str(cur.fetchone()[0])
         hashed_password = hashlib.sha512(entered_old_pass.encode() + salt.encode()).hexdigest()
         if hashed_password != correct_old_pass:
             return render_template('password_change.html', pass_error="incorrect password")
-
+        salt = uuid.uuid4().hex
         new_hashed_password = hashlib.sha512(new_pass.encode() + salt.encode()).hexdigest()
-        cur.execute('UPDATE users SET password = (%s) WHERE username = (%s)',
-                    (new_hashed_password, session['username'],))
+        cur.execute("UPDATE users SET password = '%s' WHERE username = '%s'" %
+                    (new_hashed_password, session['username']))
+        cur.execute("UPDATE users SET salt = '%s' WHERE username = '%s'" % (salt, session['username']))
         return render_template('account.html')
     except Exception as e:
         return render_template('error.html', FUCK=e)
     finally:
         if conn:
             conn.close()
+
 
 @app.route('/login')
 def login():
@@ -151,6 +182,35 @@ def login():
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
+
+
+@app.route('/password_reset', methods=['GET', 'POST'])
+def password_reset():
+    try:
+        conn = None
+        conn = getconn()
+        cur = conn.cursor()
+        cur.execute(search_path)
+        new_password = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        recipient = str(request.form['email'])
+        msg = Message(subject="Password Reset",
+                      sender="TestForCoursework@gmail.com",
+                      recipients=[recipient],
+                      body="New password : " + str(new_password))
+        mail.send(msg)
+        salt = uuid.uuid4().hex
+        new_password_hashed = hashlib.sha512(new_password.encode() + salt.encode()).hexdigest()
+        # DON'T KNOW WHY THESE WON'T EXECUTE
+        # BUT WHEN THEY DO IT WILL SHOULD ALL WORK
+        cur.execute("UPDATE users SET password = '%s' WHERE email = '%s'" % (new_password_hashed, recipient))
+        cur.execute("UPDATE users SET salt = '%s' WHERE email = '%s'" % (salt, recipient))
+        return render_template('password_reset_redirect.html', email=recipient)
+    except Exception as e:
+        return render_template('ERROR.html', FUCK=e)
+
+    finally:
+        if conn:
+            conn.close()
 
 
 @app.route('/login_user', methods=['GET', 'POST'])
@@ -164,9 +224,11 @@ def login_user():
         cur.execute(search_path)
         # Takes the salt stored with the user name, adds and hashes the password provided then checks it against the
         # stored password
-        cur.execute("SELECT password FROM users WHERE username = (%s);", (username,))
+        # cur.execute("SELECT password FROM users WHERE username = (%s);", (username,))
+        cur.execute("SELECT password FROM users WHERE username = '%s'" % username)
         stored_pass = str(cur.fetchone()[0])
-        cur.execute("SELECT salt FROM users WHERE username = (%s);", (username,))
+        # cur.execute("SELECT salt FROM users WHERE username = (%s);", (username,))
+        cur.execute("SELECT salt FROM users WHERE username = '%s'" % username)
         salt = str(cur.fetchone()[0])
         sub_hashed_password = hashlib.sha512(entered_pass.encode() + salt.encode()).hexdigest()
         if stored_pass == sub_hashed_password:
